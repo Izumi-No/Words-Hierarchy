3e24225efe71373b21bacaf2459530f801fcc1ba
Kevyn (izumi) <kevynmiguel@gmail.com>
2024-09-29 23:18:09 -0300

    refactor: separate commands in proper files

--- src/cli.ts
+++ src/cli.ts
@@ -1,85 +1,12 @@
-import type { ICommandHandler } from "./interfaces/ICommandHandler.ts";
-import type { Hierarchy } from "./models/hierarchy.ts";
-import { AnalyzerService } from "./services/analyzerService.ts";
-import { StructureParserService } from "./services/structureParserService.ts";
-import { measurePerformance } from "./utils/performaceHandler.ts";
 import fs from "node:fs";
+import process from "node:process";
+import { CommandManager } from "./utils/commandManager.ts";
+import { AnalyzeCommandHandler } from "./commands/analyzeCommand.ts";
 
-class AnalyzeCommandHandler implements ICommandHandler {
-    handleCommand(args: string[], json: Hierarchy) {
-        const depth = args.findIndex((arg) => arg === "-depth");
-        if (depth === -1) {
-            console.log("Missing argument for -depth");
-            Deno.exit(1);
-        }
-
-        if (args[depth + 1] === undefined || Number.isNaN(Number(args[depth + 1]))) {
-            console.log("Invalid depth value");
-            Deno.exit(1);
-        }
-
-        const depthValue = args[depth + 1];
-        args.splice(depth, 2);
-
-        if (Number(depthValue) < 0) {
-            console.log("Invalid depth value");
-            Deno.exit(1);
-        }
-
-        const verboseIndex = args.findIndex((arg) => arg === "-verbose");
-        if (verboseIndex === -1) {
-            console.log("Missing argument for -verbose");
-            Deno.exit(1);
-        }
-
-        args.splice(verboseIndex, 1);
-
-        const verbose = true;
-
-        if (args.length === 0) {
-            console.log("Missing phrase");
-            Deno.exit(1);
-        }
-
-        const phrase = args.join(" ").replaceAll('"', "").replaceAll("'", "");
-
-        const [structureTime, structureParser] = measurePerformance(() => {
-            return StructureParserService.fromJson(json);
-        });
-
-        const [analyzeTime, result] = measurePerformance(() => {
-            const analyzerService = new AnalyzerService(structureParser);
-            return analyzerService.analyzePhrase(phrase, Number(depthValue));
-        });
-
-        if (verbose) {
-            console.log(`Structure parsing time: ${structureTime.toFixed(3)}ms`);
-            console.log(`Analysis time: ${analyzeTime.toFixed(3)}ms`);
-        }
-
-        console.log(result);
-    }
-}
-
-// Command manager applies Open/Closed principle.
-class CommandManager {
-    private commands: { [command: string]: ICommandHandler } = {};
-
-    registerCommand(command: string, handler: ICommandHandler) {
-        this.commands[command] = handler;
-    }
-
-    executeCommand(command: string, args: string[], json: Hierarchy) {
-        const handler = this.commands[command];
-        if (!handler) {
-            console.log("Invalid command");
-            Deno.exit(1);
-        }
-        handler.handleCommand(args, json);
-    }
-}
-
-// Deno and Node CLI logic are extracted to separate classes for SRP.
+/**
+ * The Intencion of this class is to be used in Deno environment and show the diference between Node and Deno.
+ * in addiction, Deno can use the most of standard library of Node. eg.: fs, process and etc.
+ */
 class DenoCLI {
     private commandManager: CommandManager;
 
@@ -118,11 +45,10 @@ class NodeCLI {
         }
 
         const command = args.shift();
-        const json = fs.readFileSync("./dicts/hierarchy.json", "utf-8");
-        const jsonHierarchy = JSON.parse(json);
+        const json = JSON.parse(fs.readFileSync("./dicts/hierarchy.json", "utf-8"));
 
         if (command) {
-            this.commandManager.executeCommand(command, args, jsonHierarchy);
+            this.commandManager.executeCommand(command, args, json);
         }
     }
 }
---
+++ src/commands/analyzeCommand.ts
@@ -0,0 +1,61 @@
+import type { ICommandHandler } from "../interfaces/ICommandHandler.ts";
+import type { Hierarchy } from "../models/hierarchy.ts";
+import { AnalyzerService } from "../services/analyzerService.ts";
+import { StructureParserService } from "../services/structureParserService.ts";
+import { measurePerformance } from "../utils/performaceHandler.ts";
+
+export class AnalyzeCommandHandler implements ICommandHandler {
+    handleCommand(args: string[], json: Hierarchy) {
+        const depth = args.findIndex((arg) => arg === "-depth");
+        if (depth === -1) {
+            console.log("Missing argument for -depth");
+            Deno.exit(1);
+        }
+
+        if (args[depth + 1] === undefined || Number.isNaN(Number(args[depth + 1]))) {
+            console.log("Invalid depth value");
+            Deno.exit(1);
+        }
+
+        const depthValue = args[depth + 1];
+        args.splice(depth, 2);
+
+        if (Number(depthValue) < 0) {
+            console.log("Invalid depth value");
+            Deno.exit(1);
+        }
+
+        const verboseIndex = args.findIndex((arg) => arg === "-verbose");
+        if (verboseIndex === -1) {
+            console.log("Missing argument for -verbose");
+            Deno.exit(1);
+        }
+
+        args.splice(verboseIndex, 1);
+
+        const verbose = true;
+
+        if (args.length === 0) {
+            console.log("Missing phrase");
+            Deno.exit(1);
+        }
+
+        const phrase = args.join(" ").replaceAll('"', "").replaceAll("'", "");
+
+        const [structureTime, structureParser] = measurePerformance(() => {
+            return StructureParserService.fromJson(json);
+        });
+
+        const [analyzeTime, result] = measurePerformance(() => {
+            const analyzerService = new AnalyzerService(structureParser);
+            return analyzerService.analyzePhrase(phrase, Number(depthValue));
+        });
+
+        if (verbose) {
+            console.log(`Structure parsing time: ${structureTime.toFixed(3)}ms`);
+            console.log(`Analysis time: ${analyzeTime.toFixed(3)}ms`);
+        }
+
+        console.log(result);
+    }
+}
